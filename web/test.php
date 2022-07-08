38d55e388e0f37a67758f8abaf43c716978df1f8
SkylerL27 <skylerling27@gmail.com>
2022-07-08 11:37:25 +0800

    Test6

--- web/test.php
+++ web/test.php
@@ -8,9 +8,9 @@
     <!-- fix : Document does not have a meta description -->
     <meta name="description" content="This is testing for lesson.">
     <!-- fix : Document does not have a viewport-->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- fix : Document does not have a <title> -->
     
+    <!-- fix : Document does not have a <title> -->
+    <title>This is a test</title>
 </head>
 <body>
   Hello World!
