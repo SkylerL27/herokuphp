d2c66e5f20b22e58d7871707c0ca4fe8ad8c2551
SkylerL27 <skylerling27@gmail.com>
2022-07-08 12:26:52 +0800

    Acitivity4

--- web/sp.php
+++ web/sp.php
@@ -5,14 +5,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="Put your description here.">
 </head>
-<script>
-    window.addEventListener('beforeunload', (event) => {
-        if (pageHasUnsavedChanges()) {
-            event.preventDefault();
-            return event.returnValue = 'Are you sure you want to exit?';
-        }
-    });
-</script>
 <body>
   <!-- fix : Image elements do not have [alt] attributes -->
   <img src="img/wallpaper.jpg" alt="view of the ocean from a mountain top">
