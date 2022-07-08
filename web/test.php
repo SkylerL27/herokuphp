26ae929134accb70104b4d52e4ea692997623287
SkylerL27 <skylerling27@gmail.com>
2022-07-08 10:44:52 +0800

    Test3

--- web/test.php
+++ web/test.php
@@ -1,6 +1,10 @@
 <!-- fix : <html> element does not have a [lang] attribute -->
+
+
+<!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>This is a test</title>
 </head>
 <body>
