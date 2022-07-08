f61dcec8e19f75c1611cca387ab525d4e81aa8e9
SkylerL27 <skylerling27@gmail.com>
2022-07-08 15:11:11 +0800

    Acitivity4

--- web/sp.php
+++ web/sp.php
@@ -7,7 +7,6 @@
 </head>
 <body>
   <!-- fix : Image elements do not have [alt] attributes -->
-  <img src="img/wallpaper.jpg" alt="view of the ocean from a mountain top">
-  <p>To see all of our basketball videos, <a href="videos.html">click here</a>.</p>
+  <img src="img/wallpaper.jpg" alt="view of the ocean from a mountain top" style="width: 1000px, height: 1000px">
 </body>
 </html>
