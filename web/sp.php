a51b9efedbed081ccc6b24ba04ddc894642a275b
SkylerL27 <skylerling27@gmail.com>
2022-07-08 12:27:43 +0800

    Acitivity4

--- web/sp.php
+++ web/sp.php
@@ -8,5 +8,6 @@
 <body>
   <!-- fix : Image elements do not have [alt] attributes -->
   <img src="img/wallpaper.jpg" alt="view of the ocean from a mountain top">
+  <span href="https://example.com">
 </body>
 </html>
